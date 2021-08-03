215b36895f1ed0678053678a5bc9d77ad689db9e
JinYunJae <jinyun3075@naver.com>
2021-08-03 17:54:20 +0900

    JPA 외래키 연결 성공, 복합키로 문제 만들기 ~ing

--- build/resources/main/static/js/publicWork/index.js
+++ build/resources/main/static/js/publicWork/index.js
@@ -7,15 +7,16 @@ var main={
     create : function(){
             var data={
                 title : $('#title').val(),
-                sub : $('#sub').val(),
-                userid:$('#userid').val()
+                sub : $('#sub').val()
             };
 
+            var id = $('#userid').val();
+
              $.ajax({
-                type:'POST',
-                url: '/work/create',
-                dataType: 'json',
-                contentType:'application/json; charset=utf-8',
+                type : 'POST',
+                url : '/work/create/'+id,
+                dataType : 'json',
+                contentType :'application/json; charset=utf-8',
                 data : JSON.stringify(data)
             }).done(function(){
                 alert('문제집이 만들어 졌습니다!');
@@ -23,32 +24,6 @@ var main={
             }).fail(function(error){
                 alert('만들기 실패');
             });
-        },
-      update : function(){
-            var data={
-                pw : $('#pw').val(),
-                name : $('#name').val(),
-                pwcheck :$('#pwcheck').val()
-            };
-
-            var id= $('#id').val();
-            $.ajax({
-                type : 'PUT',
-                url : '/api/update/'+id,
-                dateType : 'json',
-                contentType : 'application/json; charset=utf-8',
-                data : JSON.stringify(data)
-            }).done(function(v){
-                if(v==-1){
-                    alert('비밀번호가 다릅니다.')
-                    window.location.href='/update';
-                }else{
-                    alert('정보가 수정되었습니다.');
-                    window.location.href='/update';
-                }
-            }).fail(function(error){
-                alert('정보수정 실패');
-            });
-      }
+        }
 }
 main.init();
